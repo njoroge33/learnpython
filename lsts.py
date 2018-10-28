317dee2c2d8a7f439d75516c094f8006c170c53c
Njoroge33 <johngichuhi769@gmail.com>
2018-10-28 07:39:21 -0700

    move logic to a function

--- lsts.py
+++ lsts.py
@@ -1,18 +1,9 @@
-a = [1,1,2,3,5,8,13,21,34,55,89]
+def print_less_than_num(num):
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    for i in a:
+        if i < num:
+            print(i)
 
 
-
-b =[]
-for i in a:
- if (i < 5):
-   b.append(i)
-
-   print(b)
-
-num = int(input("enter a number: "))
-for i in a:
-    if i < num:
-        print(i)
-
-
-
+num_input = int(input("enter a number: "))
+print_less_than_num(num_input)
