3c622757bda4093629a8aa634880e47d9f35b03b
Njoroge33 <johngichuhi769@gmail.com>
2018-10-28 07:17:44 -0700

    use for loop

--- lsts.py
+++ lsts.py
@@ -1,14 +1,18 @@
 a = [1,1,2,3,5,8,13,21,34,55,89]
 
-for i in a:
- if (i < 5) :
-     print (i)
 
 
 b =[]
 for i in a:
  if (i < 5):
-  b.append(i)
+   b.append(i)
 
-  print (b)
+   print(b)
+
+num = int(input("enter a number: "))
+for i in a:
+    if i < num:
+        print(i)
+
+
 
