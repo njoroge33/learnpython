53949bddc2bcb420962e77f4e23993addc919b01
njoroge33 <44545876+njoroge33@users.noreply.github.com>
2018-12-31 02:28:35 -0800

    Add files via upload

---
+++ ex 14.py
@@ -0,0 +1,19 @@
+# using a function
+def rem_duplicates(a):
+    for i in a:
+        if i not in b:
+            b.append(i)
+            return
+
+
+a = [1, 3, 3, 8, 9, 4, 8, 4, 1]
+b = []
+print(rem_duplicates(a))
+
+
+
+
+# using sets
+a = [1, 3, 3, 8, 9, 4, 8, 4, 1]
+a = set(a)
+print(a)
---
+++ ex12.py
@@ -0,0 +1,5 @@
+a =[5, 10, 15, 20, 25, 30]
+# The function prints the first and the last elements of a list
+def frst_lst(b):
+    return(a[0], a[-1])
+print(frst_lst(a))
